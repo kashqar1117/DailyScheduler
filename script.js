5df12a56fcb865c8b31b87d3686dfa4c90cb62f8
Kristina <kashqar1117@gmail.com>
2021-01-09 09:35:32 -0600

    saving

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var currentTime;
 
 var timeSlots = ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm']
 
-var planner = JSON.parse(localStorage.getItem('planner')) || [];
+var planner = JSON.parse(localStorage.getItem('planner')) || {};
 
 generateTimeSlots()
 
